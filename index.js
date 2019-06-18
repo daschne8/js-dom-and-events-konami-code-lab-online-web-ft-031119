9a3883f665d03e2ad4be8deb460eeec49ae16c0c
Daniel Schneider <daschne8@gmail.com>
2019-06-18 14:38:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   let index = 0
-  function onKeyDownHandler(e) {
+  document.addEventListener('click',function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which);
 
     if (key === code[index]) {
@@ -17,20 +17,5 @@ function init() {
     } else {
       index = 0;
     }
-  }
-}
-function onKeyDownHandler(e) {
-  const key = parseInt(e.detail || e.which);
-
-  if (key === code[index]) {
-    index++;
-
-    if (index === code.length) {
-      alert("Hurray!");
-
-      index = 0;
-    }
-  } else {
-    index = 0;
-  }
+  })
 }
